5342c4aa5303ecb35849ccfa4c750a1a8da5e7c2
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-03 09:16:33 -0500

    Set up new Rails project, updated Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeddingDress::Application.config.secret_key_base = '1a6dbd33059624bb641a3ae18d3c13b6c5c012e9ffe04033e04ecb6ccad883dee5a2266042214e488904e7ea0a676dc809a2387cadcc05840c2955f6f009a85b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeddingDress::Application.config.session_store :cookie_store, key: '_WeddingDress_session'
