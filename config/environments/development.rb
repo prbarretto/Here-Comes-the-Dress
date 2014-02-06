66c580e6c48a5389d30b824cbb22eba50a1f952f
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-06 09:33:13 -0500

    updated Devise messages and made authentication mandatory to use the app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,5 @@ WeddingDress::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
