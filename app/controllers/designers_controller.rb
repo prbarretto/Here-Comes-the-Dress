afdc8ee014ca0c9cd594c564ae7240b21af2b99b
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-05 23:11:45 -0500

    updated the controllers

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,22 +1,22 @@
-class DesignersController < ApplicationController
-	include Yelp::V2::Business::Request
+# class DesignersController < ApplicationController
+# 	include Yelp::V2::Business::Request
 
-	def index
-		@designer = Designer.new
-		@designers = Designer.all
-	end
+# 	def index
+# 		@designer = Designer.new
+# 		@designers = Designer.all
+# 	end
 
-	def show
-		@designer = Designer.find(params[:designer_id])
-		@stores = @designer.stores
-	end
+# 	def show
+# 		@designer = Designer.find(params[:designer_id])
+# 		@stores = @designer.stores
+# 	end
 
-	def self.search_yelp(yelp_id)
-		client = Yelp::Client.new
-		request = Id.new(:yelp_business_id => yelp_id)
- 		response = client.search(request)
-	end
-end
+# 	def self.search_yelp(yelp_id)
+# 		client = Yelp::Client.new
+# 		request = Id.new(:yelp_business_id => yelp_id)
+#  		response = client.search(request)
+# 	end
+# end
 	# def search_form
 	# 	# render # form to enter a designer
 	# 	# search_form.html.erb
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,18 +1,18 @@
-class StoresController < ApplicationController
+# class StoresController < ApplicationController
 
-	def index
-		if params[:designer_id]
-			designer = Designer.find(params[:designer_id])
-			@stores = designer.stores
-	  else
-			@stores = Store.all
-		end
-	end
+# 	def index
+# 		if params[:designer_id]
+# 			designer = Designer.find(params[:designer_id])
+# 			@stores = designer.stores
+# 	  else
+# 			@stores = Store.all
+# 		end
+# 	end
 
-	def show
-		@store = Store.find(params[:id])
-	end
+# 	def show
+# 		@store = Store.find(params[:id])
+# 	end
 
-	def create
-	end
-end
+# 	def create
+# 	end
+# end
