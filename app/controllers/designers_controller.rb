be1ab7de54f4567e58aae2708d2fc97c2b57700c
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-13 16:44:37 -0500

    added newrelic gem to gemfile and bundled

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,30 +1,33 @@
 class DesignersController < ApplicationController
-	include Yelp::V2::Search::Request
+  include Yelp::V2::Search::Request
 
-	def index
- 		@designer = Designer.new
- 		@designers = Designer.all
- 		render :new
-	end
+  def index
+    @designer = Designer.new
+    @designers = Designer.all
+    render :new
+  end
 
-	def new
-		@designer = Designer.new
-	end
+  def new
+    @designer = Designer.new
+  end
 
-	def create
-		designer = Designer.find(params[:id])
-		@stores = designer.stores
-		@stores_yelp = []
+  def create
+    designer = Designer.find(params[:id])
+    # I want:
+    # @stores_yelp = designer.yelp_results
 
-		client = Yelp::Client.new
+    @stores = designer.stores
+    @stores_yelp = []
 
-		@stores.each do |store|
-			request = Yelp::V2::Business::Request::Id.new(:yelp_business_id => store.yelp_id)
-			response = client.search(request)
-			@stores_yelp << response
-		end
-		render :show
-	end
+    client = Yelp::Client.new
+
+    @stores.each do |store|
+      request = Yelp::V2::Business::Request::Id.new(:yelp_business_id => store.yelp_id)
+      response = client.search(request)
+      @stores_yelp << response
+    end
+    render :show
+  end
 
 
 end
